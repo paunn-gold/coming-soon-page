67162d7782786b069adfa586fe614771f5ee1f9d
Revanth <revanthvishal@gmail.com>
2020-08-16 14:57:46 +0530

    Feature: Color scheme switch & Code cleanup

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("switch-color").addEventListener("click", colorSwitch);
+let swap;
+if (window.matchMedia("prefers-color-scheme: light").matches) {
+  swap = 1;
+  document.getElementById("logo").src = "assets/light-logo.png";
+} else {
+  swap = 0;
+  document.getElementById("logo").src = "assets/dark-logo.png";
+}
+function colorSwitch() {
+  if (swap == 1) {
+    document.body.style.backgroundColor = "#000000";
+    document.body.style.color = "#f5f3ef";
+    document.getElementsByTagName("a")[0].style.color = "#999999";
+    document.getElementById("logo").src = "assets/dark-logo.png";
+    swap = 0;
+  } else if (swap == 0) {
+    document.body.style.backgroundColor = "transparent";
+    document.body.style.color = "#262626";
+    document.getElementsByTagName("a")[0].style.color = "#666666";
+    document.getElementById("logo").src = "assets/light-logo.png";
+    swap = 1;
+  }
+}
