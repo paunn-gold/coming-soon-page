2b7a80e4a2c25e84f63ae7d45801d274dbb762c5
Revanth <revanthvishal@gmail.com>
2020-08-17 16:43:43 +0530

    Feature: Jiggle effect & Code cleanup

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 document.getElementById("switch-color").addEventListener("click", colorSwitch);
-let swap;
+window
+  .matchMedia("(prefers-color-scheme: light)")
+  .addListener((e) => e.matches && colorSwitch());
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addListener((e) => e.matches && colorSwitch());
 if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-  swap = 1;
-  document.getElementById("logo").src = "assets/light-logo.png";
+  document.getElementById("logo-overlay").style.backgroundColor = "#f5f3ef";
 } else {
-  swap = 0;
-  document.getElementById("logo").src = "assets/dark-logo.png";
+  document.getElementById("logo-overlay").style.backgroundColor = "#161616";
 }
 function colorSwitch() {
-  if (swap == 1) {
+  if (
+    document.getElementById("logo-overlay").style.backgroundColor ==
+    "rgb(245, 243, 239)"
+  ) {
     document.body.style.backgroundColor = "#000000";
     document.body.style.color = "#f5f3ef";
     document.getElementsByTagName("a")[0].style.color = "#999999";
-    document.getElementById("logo").src = "assets/dark-logo.png";
-    swap = 0;
-  } else if (swap == 0) {
+    document.getElementById("logo-overlay").style.backgroundColor = "#161616";
+  } else if (
+    document.getElementById("logo-overlay").style.backgroundColor ==
+    "rgb(22, 22, 22)"
+  ) {
     document.body.style.backgroundColor = "transparent";
-    document.body.style.color = "#262626";
+    document.body.style.color = "#161616";
     document.getElementsByTagName("a")[0].style.color = "#666666";
-    document.getElementById("logo").src = "assets/light-logo.png";
-    swap = 1;
+    document.getElementById("logo-overlay").style.backgroundColor = "#f5f3ef";
   }
 }
