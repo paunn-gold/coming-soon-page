1602ec5b9ccd0b175abcbdeaddc4fa268a9665c6
Revanth <revanth@paunn.com>
2020-08-19 16:31:17 +0530

    Fix for color glitch on iOS devices & Code cleanup

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 document.getElementById("switch-color").addEventListener("click", colorSwitch);
-window
-  .matchMedia("(prefers-color-scheme: light)")
-  .addListener((e) => e.matches && colorSwitch());
-window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addListener((e) => e.matches && colorSwitch());
-if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-  document.getElementById("logo-overlay").style.backgroundColor = "#f5f3ef";
-} else {
-  document.getElementById("logo-overlay").style.backgroundColor = "#161616";
-}
+window.matchMedia("(prefers-color-scheme: light)").addListener(function (e) {
+  document.body.removeAttribute("style");
+  document.getElementsByTagName("a")[0].removeAttribute("style");
+  document.getElementsByTagName("hr")[0].removeAttribute("style");
+  document.getElementById("logo-overlay").removeAttribute("style");
+  return e.matches;
+});
+window.matchMedia("(prefers-color-scheme: dark)").addListener(function (e) {
+  document.body.removeAttribute("style");
+  document.getElementsByTagName("a")[0].removeAttribute("style");
+  document.getElementsByTagName("hr")[0].removeAttribute("style");
+  document.getElementById("logo-overlay").removeAttribute("style");
+  return e.matches;
+});
 function colorSwitch() {
   if (
-    document.getElementById("logo-overlay").style.backgroundColor ==
-    "rgb(245, 243, 239)"
+    window
+      .getComputedStyle(document.body, null)
+      .getPropertyValue("background-color") == "rgb(255, 255, 255)"
   ) {
-    document.body.style.backgroundColor = "#000000";
-    document.body.style.color = "#f5f3ef";
-    document.getElementsByTagName("a")[0].style.color = "#bbbbbb";
-    document.getElementsByTagName("hr")[0].style.backgroundColor = "#d1d3d4";
-    document.getElementById("logo-overlay").style.backgroundColor = "#161616";
+    document.body.style.backgroundColor = "rgb(0, 0, 0)"; //#000000
+    document.body.style.color = "rgb(245, 243, 239)"; //#f5f3ef
+    document.getElementsByTagName("a")[0].style.color = "rgb(187, 187, 187)"; //#bbbbbb
+    document.getElementsByTagName("hr")[0].style.backgroundColor =
+      "rgb(209, 211, 212)"; //#d1d3d4
+    document.getElementById("logo-overlay").style.backgroundColor =
+      "rgb(22, 22, 22)"; //#161616
   } else if (
-    document.getElementById("logo-overlay").style.backgroundColor ==
-    "rgb(22, 22, 22)"
+    window
+      .getComputedStyle(document.body, null)
+      .getPropertyValue("background-color") == "rgb(0, 0, 0)"
   ) {
-    document.body.style.backgroundColor = "transparent";
-    document.body.style.color = "#161616";
-    document.getElementsByTagName("a")[0].style.color = "#444444";
-    document.getElementsByTagName("hr")[0].style.backgroundColor = "#262626";
-    document.getElementById("logo-overlay").style.backgroundColor = "#f5f3ef";
+    document.body.style.backgroundColor = "rgb(255, 255, 255)"; //#ffffff
+    document.body.style.color = "rgb(22, 22, 22)"; //#161616
+    document.getElementsByTagName("a")[0].style.color = "rgb(68, 68, 68)"; //#444444
+    document.getElementsByTagName("hr")[0].style.backgroundColor =
+      "rgb(38, 38, 38)"; //#262626
+    document.getElementById("logo-overlay").style.backgroundColor =
+      "rgb(245, 243, 239)"; //#f5f3ef
   }
 }
